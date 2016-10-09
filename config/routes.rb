10b70cb64c2be0e556851425748168db2a4732b9
Quang Minh <thanbathai@gmail.com>
2016-10-09 17:39:25 +0700

    signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,21 @@
 Rails.application.routes.draw do
+  resources :users
+  resources :users
+  get 'show' => 'users#show'
+
+  get 'signup' => 'users#new'
+
   get 'static_pages/home'
 
   get 'static_pages/help'
-
+  get 'static_pages/login'
+  get 'login' => 'static_pages#login'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'static_pages#home'
-
+  resources :users
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
